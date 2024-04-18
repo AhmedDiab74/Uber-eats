48783fa1bd5047be1e26ed06f96abd4f877c32b1
Your Name <you@example.com>
2024-04-18 13:37:32 +0200

    Auth Screens

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'package:ubereats/view/auth_sceen/mobile_login_screen.dart';
+import 'package:ubereats/view/auth_sceen/otp_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(builder: (context, _, __) {
+      return const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          home: OTPScreen());
+    });
+  }
+}
